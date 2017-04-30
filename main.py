8cb6f00c6774c52e287df065bad98f71c721ae3a
Heather Cunningham <kcredwolf@gmail.com>
2017-04-30 17:48:31 -0500

    Loaded backup files, updated code. Needs solution for Undefined Error.

--- main.py
+++ main.py
@@ -15,11 +15,77 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),autoescape=True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t=jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    entry = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPageHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        self.redirect("/blog")
+
+class BlogPage(Handler):
+    def render_blog(self, title="", entry="", error=""):
+        posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC LIMIT 5")
+        self.render('blog.html', title=title, entry=entry, error=error, posts=posts)
+
+    def get(self):
+        page = self.request.get("page","1")
+        self.render_blog(page)
+
+
+class NewPost(Handler):
+
+    def render_form(self, title="", entry="", error=""):
+        self.render("newpost.html", title=title, entry=entry, error=error)
+
+    def get(self):
+        self.render_form()
+
+    def post(self):
+        title = self.request.get("title")
+        entry = self.request.get("entry")
+
+        if title and entry:
+            b = BlogPost(title=title, entry=entry)
+            b.put()
+            postroute = BlogPost.key(b).id()
+            self.redirect("/blog/" + str(postroute))
+        else:
+            error="Please enter a title and text!"
+            self.render_form(title=title, entry=entry, error=error)
+
+class ViewPost(Handler):
+    def get(self, id):
+        id = int(id)
+        blogpost= BlogPost.get_by_id(id)
+
+        self.render("viewpost.html", blogpost=blogpost, id=id)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPageHandler),
+    ('/blog', BlogPage),
+    ('/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPost),
 ], debug=True)
